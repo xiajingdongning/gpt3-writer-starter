4bdc0d02cec05646580162b33f202dc99d888b2c
YangliLiu <liu.yangl@northeastern.edu>
2022-12-03 20:53:57 -0500

    First Version
    The webpage build finished

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+﻿// JavaScript source code
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `Answer the question with three quotes from famous people of Chan Sect and Aesthetics, 
+Graeco philosophy and mythology, and religion in its original language and then display its English translation and the name of the famous people
+Answer:`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
